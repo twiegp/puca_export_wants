1e8a4d27f2ecea21b5716e7d53d285169eaee739
twiegp <twiegp@gmail.com>
2017-06-03 18:01:09 -0700

    Text edit

--- puca-export.js
+++ puca-export.js
@@ -2,7 +2,7 @@
 
   alert('Ready to export.\n'
     + 'This includes loading all your haves which may take some time.\n'
-    + 'A download button will appear in the top left corenr of the screen when done.');
+    + 'A download button will appear in the top left corner of the screen when done.');
 
   var conditionMap = {
     'NM/M': 'Near Mint',
